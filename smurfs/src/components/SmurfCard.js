668da4c53a99202e518bd2729b97319ec14a46ed
Ethan-Edgerton <Ethan.E.Edgerton@gmail.com>
2020-05-01 21:02:35 -0500

    MVP almost met, has one bug

---
+++ smurfs/src/components/SmurfCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+const SmurfCard = ({ smurf, removeSmurf }) => {
+  return (
+    <Card>
+      <h2>Meet, {smurf.name}!</h2>
+      <p>
+        {smurf.name} is {smurf.age} years old and is {smurf.height} feet tall!
+      </p>
+      <button onClick={() => removeSmurf(smurf.id)}>Remove Smurf</button>
+    </Card>
+  );
+};
+
+export default SmurfCard;
+
+const Card = styled.div`
+  background-color: dodgerblue;
+  width: 60%;
+  border-radius: 5px;
+  padding: 10px 0;
+  margin: 10px auto;
+  color: white;
+  text-shadow: black 1px 1px 1px;
+  button {
+    background: dodgerblue;
+    color: white;
+    border: solid black 3px;
+    text-shadow: black 1px 1px 1px;
+    border-radius: 4px;
+    padding: 10px;
+  }
+`;
